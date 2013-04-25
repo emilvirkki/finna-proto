6ebab2fe5ad19b45c9ab6df26fb49e64a7a9077e
Emil Virkki <emil.virkki@gmail.com>
2013-04-26 00:36:07 +0300

    Fixed 404:s

---
+++ 404page.php
@@ -0,0 +1,9 @@
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Näkymää ei ole toteutettu</title>
+  </head>
+  <body>
+    Tätä näkymää ei ole toteutettu testaamassasi prototyypissä. Voit palata takaisin ja kokeilla jotakin muuta linkkiä.
+  </body>
+</html>
